84d48a54221cf183db956632bb1099422635dcf8
Ariq Daffa Athallah Putra <ariqdaffaathallah@gmail.com>
2022-01-10 20:40:42 +0700

    6 - toggle project status

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="project">
+    <div class="project" :class="{ complete: project.complete }">
         <div class="actions">
             <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
             <div class="icons">
                 <span class="material-icons">edit</span>
                 <span @click="deleteProject" class="material-icons">delete</span>
-                <span class="material-icons">done</span>
+                <span @click="toggleComplete" class="material-icons tick">done</span>
             </div>
         </div>
         <div class="details" v-if="showDetails">
@@ -29,6 +29,14 @@ export default {
                 .then(() => this.$emit('delete', this.project.id))
                 .catch(err => console.log(err))
         },
+        toggleComplete() {
+            fetch(this.uri, {
+                method: 'PATCH',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ complete: !this.project.complete })
+            }).then(() => this.$emit('complete', this.project.id))
+
+        }
     },
 };
 </script>
@@ -41,6 +49,12 @@ export default {
     border-radius: 4px;
     box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
     border-left: 4px solid #e90074;
+}
+.project.complete {
+    border-left: 4px solid #00ce89;
+}
+.project.complete .tick {
+    color: #00ce89;
 }
 h3 {
     cursor: pointer;
